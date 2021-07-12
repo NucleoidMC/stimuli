44f554eb0638f51d3cdbb1ae82171a64aff12ecd
Patbox <39821509+Patbox@users.noreply.github.com>
2021-07-12 17:03:28 +0200

    Add packet events (#5)

---
+++ src/main/java/xyz/nucleoid/stimuli/mixin/player/ClientConnectionMixin.java
@@ -0,0 +1,28 @@
+package xyz.nucleoid.stimuli.mixin.player;
+
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.Packet;
+import net.minecraft.network.listener.PacketListener;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.nucleoid.stimuli.Stimuli;
+import xyz.nucleoid.stimuli.event.player.PlayerC2SPacketEvent;
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin {
+    @Inject(method = "handlePacket", at = @At("HEAD"), cancellable = true)
+    private static void onPacket(Packet<?> packet, PacketListener listener, CallbackInfo ci) {
+        if (listener instanceof ServerPlayNetworkHandler handler) {
+            try (var invokers = Stimuli.select().forEntity(handler.player)) {
+                var result = invokers.get(PlayerC2SPacketEvent.EVENT).onPacket(handler.player, packet);
+                if (result == ActionResult.FAIL) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}
--- src/main/java/xyz/nucleoid/stimuli/mixin/player/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/xyz/nucleoid/stimuli/mixin/player/ServerPlayNetworkHandlerMixin.java
@@ -1,14 +1,19 @@
 package xyz.nucleoid.stimuli.mixin.player;
 
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.nucleoid.stimuli.Stimuli;
+import xyz.nucleoid.stimuli.event.player.PlayerS2CPacketEvent;
 import xyz.nucleoid.stimuli.event.player.PlayerSwingHandEvent;
 
 @Mixin(ServerPlayNetworkHandler.class)
@@ -21,6 +26,16 @@ public class ServerPlayNetworkHandlerMixin {
         var hand = packet.getHand();
         try (var invokers = Stimuli.select().forEntity(this.player)) {
             invokers.get(PlayerSwingHandEvent.EVENT).onSwingHand(this.player, hand);
+        }
+    }
+
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
+    private void onPacket(Packet<?> packet, GenericFutureListener<? extends Future<? super Void>> listener, CallbackInfo ci) {
+        try (var invokers = Stimuli.select().forEntity(this.player)) {
+            var result = invokers.get(PlayerS2CPacketEvent.EVENT).onPacket(this.player, packet);
+            if (result == ActionResult.FAIL) {
+                ci.cancel();
+            }
         }
     }
 }
