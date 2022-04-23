563eaa80e8ffd9476483763e165e6138ee6ad6e9
Zailer43 <63565983+Zailer43@users.noreply.github.com>
2022-04-23 07:17:50 -0300

    Add DispenserUseEvent (#17)
    * Add DispenserUseEvent
    
    * Suggested changes
    * Rename DispenseUseEvent to DispenserActivateEvent
    * Change LocalCapture from CAPTURE_FAILSOFT to CAPTURE_FAILHARD
    * Rename parameter itemStack to stackToDispense
    * Change BlockPointerImpl parameter type to BlockPointer

---
+++ src/main/java/xyz/nucleoid/stimuli/event/block/DispenserActivateEvent.java
@@ -0,0 +1,38 @@
+package xyz.nucleoid.stimuli.event.block;
+
+import net.minecraft.block.entity.DispenserBlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
+import xyz.nucleoid.stimuli.event.StimulusEvent;
+
+/**
+ * Called when a {@link DispenserBlockEntity} is activated and is not empty.
+ *
+ * <p>Upon return:
+ * <ul>
+ * <li>{@link ActionResult#SUCCESS} cancels further processing and allows the use.
+ * <li>{@link ActionResult#FAIL} cancels further processing and cancels the use.
+ * <li>{@link ActionResult#PASS} moves on to the next listener.</ul>
+ * <p>
+ * If all listeners return {@link ActionResult#PASS}, the use succeeds and proceeds with normal logic.
+ */
+public interface DispenserActivateEvent {
+    StimulusEvent<DispenserActivateEvent> EVENT = StimulusEvent.create(DispenserActivateEvent.class, ctx -> (world, pos, blockPointer, dispenserBlockEntity, slot, stackToDispense) -> {
+        try {
+            for (var listener : ctx.getListeners()) {
+                var result = listener.onActivate(world, pos, blockPointer, dispenserBlockEntity, slot, stackToDispense);
+                if (result != ActionResult.PASS) {
+                    return result;
+                }
+            }
+        } catch (Throwable t) {
+            ctx.handleException(t);
+        }
+        return ActionResult.PASS;
+    });
+
+    ActionResult onActivate(ServerWorld world, BlockPos pos, BlockPointer blockPointer, DispenserBlockEntity dispenserBlockEntity, int slot, ItemStack stackToDispense);
+}
---
+++ src/main/java/xyz/nucleoid/stimuli/mixin/block/DispenserBlockMixin.java
@@ -0,0 +1,41 @@
+package xyz.nucleoid.stimuli.mixin.block;
+
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.entity.DispenserBlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPointerImpl;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import xyz.nucleoid.stimuli.Stimuli;
+import xyz.nucleoid.stimuli.event.block.DispenserActivateEvent;
+
+@Mixin(DispenserBlock.class)
+public class DispenserBlockMixin {
+
+    @Inject(
+            method = "dispense",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/DispenserBlock;getBehaviorForItem(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/block/dispenser/DispenserBehavior;",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
+    )
+    private void useDispenser(ServerWorld world, BlockPos pos, CallbackInfo ci, BlockPointerImpl blockPointerImpl, DispenserBlockEntity dispenserBlockEntity, int slot, ItemStack itemStack) {
+        var events = Stimuli.select();
+
+        try (var invokers = events.at(world, pos)) {
+            var result = invokers.get(DispenserActivateEvent.EVENT).onActivate(world, pos, blockPointerImpl, dispenserBlockEntity, slot, itemStack);
+            if (result == ActionResult.FAIL) {
+                ci.cancel();
+            }
+        }
+    }
+}
