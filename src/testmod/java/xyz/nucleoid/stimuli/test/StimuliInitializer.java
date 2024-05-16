d7740a3d39b257fca87311b0e62d9c8d7ddeecbf
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-05-16 17:02:23 -0300

    chore: Add test for explosions

--- src/testmod/java/xyz/nucleoid/stimuli/test/StimuliInitializer.java
+++ src/testmod/java/xyz/nucleoid/stimuli/test/StimuliInitializer.java
@@ -3,6 +3,8 @@ package xyz.nucleoid.stimuli.test;
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
@@ -10,12 +12,14 @@ import xyz.nucleoid.stimuli.Stimuli;
 import xyz.nucleoid.stimuli.event.block.FlowerPotModifyEvent;
 import xyz.nucleoid.stimuli.event.entity.EntityShearEvent;
 import xyz.nucleoid.stimuli.event.projectile.ArrowFireEvent;
+import xyz.nucleoid.stimuli.event.world.ExplosionDetonatedEvent;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public final class StimuliInitializer implements ModInitializer {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static ActionResult result = ActionResult.PASS;
+    private static MinecraftServer server;
 
     @Override
     public void onInitialize() {
@@ -31,6 +35,10 @@ public final class StimuliInitializer implements ModInitializer {
             dispatcher.register(command);
         });
 
+        ServerLifecycleEvents.SERVER_STARTING.register(server1 -> server = server1);
+
+        ServerLifecycleEvents.SERVER_STOPPED.register(server1 -> server = null);
+
         Stimuli.global().listen(FlowerPotModifyEvent.EVENT, (player, hand, hitResult) -> {
             player.sendMessage(Text.literal("FlowerPotModifyEvent"));
             return result;
@@ -41,11 +49,14 @@ public final class StimuliInitializer implements ModInitializer {
         });
         Stimuli.global().listen(EntityShearEvent.EVENT, (entity, player, hand, pos) -> {
             if (player == null) {
-                LOGGER.info("EntityShearEvent: {}", entity.getName().toString());
+                server.sendMessage(Text.literal("EntityShearEvent: " + entity.getName().getString()));
             } else {
                 player.sendMessage(Text.literal("EntityShearEvent: " + entity.getName().getString()));
             }
             return result;
+        });
+        Stimuli.global().listen(ExplosionDetonatedEvent.EVENT, (explosion, particles) -> {
+            server.sendMessage(Text.literal("ExplosionDetonatedEvent: " + explosion.getDestructionType().name()));
         });
     }
 }
