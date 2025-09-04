02937d03d6e7e56923135edf6498b076151c98a9
Patbox <39821509+Patbox@users.noreply.github.com>
2025-09-04 10:06:58 +0200

    Fix RangedWeaponItemMixin and update to 1.21.8

--- src/main/java/xyz/nucleoid/stimuli/mixin/projectile/RangedWeaponItemMixin.java
+++ src/main/java/xyz/nucleoid/stimuli/mixin/projectile/RangedWeaponItemMixin.java
@@ -1,6 +1,12 @@
 package xyz.nucleoid.stimuli.mixin.projectile;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -10,58 +16,39 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.RangedWeaponItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.nucleoid.stimuli.Stimuli;
 import xyz.nucleoid.stimuli.duck.PassBowUseTicks;
 import xyz.nucleoid.stimuli.event.EventResult;
 import xyz.nucleoid.stimuli.event.projectile.ArrowFireEvent;
 
-import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(RangedWeaponItem.class)
 public abstract class RangedWeaponItemMixin implements PassBowUseTicks {
-    @Unique private ThreadLocal<ProjectileEntity> projectile = new ThreadLocal<>();
 
     @Shadow
     protected abstract ProjectileEntity createArrowEntity(World world, LivingEntity shooter, ItemStack weaponStack, ItemStack projectileStack, boolean critical);
 
-    @Inject(
+    @WrapOperation(
       method = "shootAll",
-      at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileEntity;spawn(Lnet/minecraft/entity/projectile/ProjectileEntity;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;Ljava/util/function/Consumer;)Lnet/minecraft/entity/projectile/ProjectileEntity;"),
-      cancellable = true
+      at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileEntity;spawn(Lnet/minecraft/entity/projectile/ProjectileEntity;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;Ljava/util/function/Consumer;)Lnet/minecraft/entity/projectile/ProjectileEntity;")
     )
-    private void onFireArrow(
-      ServerWorld world,
-      LivingEntity shooter,
-      Hand hand,
-      ItemStack tool,
-      List<ItemStack> projectiles,
-      float speed,
-      float divergence,
-      boolean critical,
-      @Nullable LivingEntity target,
-      CallbackInfo ci,
-      @Local(ordinal = 1) ItemStack projectileStack
+    private ProjectileEntity onFireArrow(
+            ProjectileEntity projectile, ServerWorld world, ItemStack projectileStack, Consumer<ProjectileEntity> beforeSpawn, Operation<ProjectileEntity> original,
+            @Local(argsOnly = true, ordinal = 0) LivingEntity shooter, @Local(argsOnly = true) ItemStack tool, @Share("damage") LocalBooleanRef damage
     ) {
         if (!(shooter instanceof ServerPlayerEntity player)) {
-            return;
+            return null;
         }
-
-        ProjectileEntity projectile = this.createArrowEntity(world, shooter, tool, projectileStack, critical);
-        this.projectile.set(projectile);
 
         Item projectileItem = projectileStack.getItem();
         if (!(projectileItem instanceof ArrowItem item) || !(projectile instanceof PersistentProjectileEntity persistentProjectile)) {
-            return;
+            damage.set(true);
+            return original.call(projectile, world, projectileStack, beforeSpawn);
         }
 
         try (var invokers = Stimuli.select().forEntity(player)) {
@@ -69,18 +56,16 @@ public abstract class RangedWeaponItemMixin implements PassBowUseTicks {
               .onFireArrow(player, tool, item, this.stimuli$getLastRemainingUseTicks(), persistentProjectile);
 
             if (result == EventResult.DENY) {
-                ci.cancel();
+                damage.set(false);
+                return projectile;
             }
         }
+        damage.set(true);
+        return original.call(projectile, world, projectileStack, beforeSpawn);
     }
 
-    @ModifyArg(
-      method = "shootAll",
-      at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileEntity;spawn(Lnet/minecraft/entity/projectile/ProjectileEntity;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/item/ItemStack;Ljava/util/function/Consumer;)Lnet/minecraft/entity/projectile/ProjectileEntity;")
-    )
-    private ProjectileEntity useStoredProjectile(ProjectileEntity original) {
-        ProjectileEntity stored = this.projectile.get();
-        this.projectile.set(null);
-        return stored != null ? stored : original;
+    @WrapWithCondition(method = "shootAll", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/EquipmentSlot;)V"))
+    private boolean cancelDamageIfNoProjectile(ItemStack instance, int amount, LivingEntity entity, EquipmentSlot slot, @Share("damage") LocalBooleanRef damage) {
+        return damage.get();
     }
 }
